be155644a118774d6c24163421cd421f38f0d8e4
Floppy-vK <115089100+Floppy-vK@users.noreply.github.com>
2022-11-23 15:23:27 +0100

    Added Exceptions
    Also did some bugfixing

--- classify_data.py
+++ classify_data.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[11]:
 
 
 #basic libraries
 import joblib
-# import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 
@@ -21,17 +19,33 @@ from skimage.transform import resize
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
-#take input in form of (path to image file, text saying what classifier should be used (i.e. bird_audio, bird_image, insect_audio, insect_image))
-def classify(image_path, classifier):
+
+#take input in form of (path to data file, text saying what classifier should be used (i.e. bird_audio, bird_image, insect_audio, insect_image))
+def classify(data_path, classifier):
+    if not classifier in ["bird_audio", "bird_image", "insect_audio", "insect_image"]:
+        raise ValueError("Given classifier must be one of: bird_audio, bird_image, insect_audio, insect_image")
+    classifier = str(classifier)
     #load saved model
     model = joblib.load(classifier + "_model.pkl")
-    
-    #do weird stuff to be able to resize image, do PCA, and input into model
-    img_path = image_path
+    img_path = data_path
     img = mpimg.imread(img_path)
-    img = resize(img, (224,224))
-    img_np = np.array([img])
-    img_rs = img_np.reshape(1, 224 * 224 * 3)
+    
+    if classifier in ["bird_audio", "insect_audio"]:
+        try:
+            img = resize(img, (217, 334))
+            img_np = np.array([img])
+            img_rs = img_np.reshape(1, 217 * 334 * 4)
+        except:
+            raise Exception("Something went wrong, most likely the input spectrogram was of incorrect shape/dimensions.")
+        
+    elif classifier in ["bird_image","insect_image"]:
+        try:
+            img = resize(img, (224,224))
+            img_np = np.array([img])
+            img_rs = img_np.reshape(1, 224 * 224 * 3)
+        except:
+            raise Exception("Something went wrong, most likely the input image was of incorrect shape/dimensions.")
+        
     pca = joblib.load(classifier + "_pca.pkl")
     img_pca = pca.transform(img_rs)
     
